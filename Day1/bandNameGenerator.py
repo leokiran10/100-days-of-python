ea7acac6013dc18ff92127b495d518508b2a8677
Kiarn Pandey <kiranpaney1899@gmail.com>
2024-12-31 14:47:04 +0545

    Completed Day1

---
+++ Day1/bandNameGenerator.py
@@ -0,0 +1,6 @@
+#Band Name Gererator
+
+print("Welcome to the bane name generator.")
+location = input("Which city did you grow up in?\n")
+animal = input("What is the name of pet?\n")
+print("Your band name is " + location + " " + animal)
---
+++ Day1/inputFunction.py
@@ -0,0 +1,2 @@
+# This is Comment.
+print("Hello " + input("What is your name?")+ "!")
