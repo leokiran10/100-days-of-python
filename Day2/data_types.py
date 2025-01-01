a788d2fc34d213148492344a6c92aa9fdfbc6e61
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-01 16:31:26 +0545

    Data Types Practice

---
+++ Day2/data_types.py
@@ -0,0 +1,27 @@
+# # Data Types
+
+# # String
+# "Hello Kiran"
+# # print("Hello"[0])
+
+# # Intiger
+# 123
+# print(12 +8)
+
+# #Folat
+# 3.14159
+
+# # Boolean
+# True
+# False
+
+a = 123
+b = 123.123
+c = "Hello"
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+newA = str(a)
+print(type(newA))
---
+++ Day2/sum_of_Input.py
@@ -0,0 +1,7 @@
+# Write a program that adds the digits in a 2 digit number.
+
+two_digit_number = input("Type a two digit Number:")
+
+number = int(two_digit_number[0]) + int(two_digit_number[1])
+
+print("Sum of two digit number is ", number)
