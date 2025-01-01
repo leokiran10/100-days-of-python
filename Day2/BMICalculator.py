b4fc386e17dade7bd953ba9c5fd22be9a24ede00
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-01 17:57:46 +0545

    Added solution of BMI Calculator

---
+++ Day2/BMICalculator.py
@@ -0,0 +1,8 @@
+# BMI Calculator
+
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg:"))
+
+bmi = int((weight/height**2))
+
+print("Your BMI is", bmi)
