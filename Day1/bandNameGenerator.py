0f597f4b1f4a00b5ce7fcacb30142965b562d39a
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-03 00:28:20 +0545

    Addded Solution of Band Name Generator

--- Day1/bandNameGenerator.py
+++ Day1/bandNameGenerator.py
@@ -2,4 +2,4 @@
 print("Welcome to the bane name generator.")
 location = input("Which city did you grow up in?\n")
 animal = input("What is the name of pet?\n")
-print("Your band name is " + location + "" + animal)
+print("Your band name is " + location + " " + animal + ".")
