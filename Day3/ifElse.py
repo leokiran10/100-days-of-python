b16ff36088a344de2a44cc018c11329cddf38e95
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-03 13:32:22 +0545

    Learned if-else statement

---
+++ Day3/ifElse.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride to the rollercoaster!")
+else:
+    print("Sorry, you to grow taller before you can ride.")
