430148c1116b4e3182341cbee9c16b181312bc0d
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-03 13:44:20 +0545

    Added solution of Odd-Even Checker

---
+++ Day3/oddEvenCheck.py
@@ -0,0 +1,8 @@
+# Write a program that works out whetherif a given number is an odd or even number.
+
+number = int(input("Which number doyou want to check? "))
+
+if number % 2 == 1:
+    print("This is a odd number.")
+else:
+    print("This is a even number.")
