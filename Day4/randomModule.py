cf64b6474c5c7ff0d5d06f8e09e89b5dd42e2a8f
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-04 21:50:09 +0545

    Learned about random module

---
+++ Day4/randomModule.py
@@ -0,0 +1,10 @@
+import random
+
+random_intiger = random.randint(1, 100)
+print(random_intiger)
+
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
