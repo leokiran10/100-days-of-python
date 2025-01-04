eb3ea80f91dab1b8b0251a70ac711fe4d90b54b3
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-04 23:03:42 +0545

    Added solution of random name peaker using split string

---
+++ Day4/splitString.py
@@ -0,0 +1,10 @@
+import random
+
+names_string = input("Gime me everybody's names, seperated by a comma. ")
+names = names_string.split(", ")
+# print(names)
+
+x = len(names)
+random_choice = random.randint(0, x-1)
+selected_person = names[random_choice]
+print(f"{selected_person} will pay the bill today!")
