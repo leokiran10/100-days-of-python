bef07471170d7b2d36ad78dd4e822fa0b078dd95
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-04 14:57:45 +0545

    Added solution of pizaa order calculator

--- Day3/pizzaOrder.py
+++ Day3/pizzaOrder.py
@@ -7,13 +7,10 @@ extra_cheese = input("Do you want extra cheese? Y ir N: ")
 
 if size == "S" or "s":
     bill = bill + 15
-    print("The cost of pizza is $15.")
 elif size == "M" or "m":
     bill = bill + 20
-    print("The cost of pizza is $20.")
 elif size == "L" or "l":
     bill = bill + 25
-    print("The cost of pizza is $25.")
 
 if add_pepperoni == "Y" or "y":
     if size == "S" or "s":
