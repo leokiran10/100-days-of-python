d3bfad4a7333b0cdfbc49273ae799a9093cade02
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-04 22:47:41 +0545

    Learned about Lists

---
+++ Day4/list.py
@@ -0,0 +1,7 @@
+states_of_nepal = ["Gandaki", "Lumbini", "Karnali", "Sudurpshim","Koshi", "Madesh","Bagmati"]
+
+print(states_of_nepal[0])
+print(states_of_nepal[-1])
+
+states_of_nepal.append("Austrilia")
+print(states_of_nepal)
