0cda6eeea61b7d8a01e844403e5e8024dc6523c7
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-10 17:42:11 +0545

    Learned about function

---
+++ Day8/function.py
@@ -0,0 +1,12 @@
+def greet():
+    name = input("What is your name? ")
+    print(f"Good Morning {name}!")
+
+greet()
+
+
+# Function with input
+def greet_with_name(name):
+    print(f"Good Morning {name}!")
+
+greet_with_name('LEO')
