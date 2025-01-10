538e7f852a51d44a84378b880567e8b21a0b0359
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-10 17:51:33 +0545

    Learned about function with Multiple inputs

--- Day8/function.py
+++ Day8/function.py
@@ -1,12 +1,19 @@
-def greet():
-    name = input("What is your name? ")
-    print(f"Good Morning {name}!")
+# def greet():
+#     name = input("What is your name? ")
+#     print(f"Good Morning {name}!")
 
-greet()
+# greet()
 
 
-# Function with input
-def greet_with_name(name):
-    print(f"Good Morning {name}!")
+# # Function with input
+# def greet_with_name(name):
+#     print(f"Good Morning {name}!")
 
-greet_with_name('LEO')
+# greet_with_name('LEO')
+
+# Funcion with more than one inputs
+def greet_with(name, location):
+    print(f"Hello {name}.")
+    print(f"Are you from {location}?")
+
+greet_with("Kiran", "Mirmee")
