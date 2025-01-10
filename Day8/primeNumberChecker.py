7f37373435f73917b8ae16d29a39c36a6fd6e694
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-10 18:59:04 +0545

    Added solution of prime number checker

---
+++ Day8/primeNumberChecker.py
@@ -0,0 +1,13 @@
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It is a prime number.")
+    else: 
+        print("It is not a prime number.")
+        
+n = int(input("Which number do you want to check: "))
+prime_checker(number = n)
