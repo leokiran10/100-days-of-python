df0d2f285f52971c5c5fb417fdb0fa33f9038da7
Kiarn Pandey <kiranpaney1899@gmail.com>
2025-01-30 12:47:39 +0545

    Learned about function with outputs

---
+++ Day10/functionsWithOutput.py
@@ -0,0 +1,9 @@
+# Functions with Outputs
+
+def format_name(f_name, l_name):
+    final_name = f_name.title() + l_name.title()
+    return final_name
+
+formated_name = format_name("KIRAN", " pandey")
+print(formated_name)
+    
